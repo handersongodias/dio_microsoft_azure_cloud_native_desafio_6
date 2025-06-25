8e401208ceb0256c8ef6a433b3966e612864248e
handersongodias <handersondias@gmail.com>
2025-06-25 02:30:13 -0300

    Add files via upload

---
+++ arquivos/functions/fnPayment/FnPayment.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Data;
+using System.Linq.Expressions;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using fnPayment.Model;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace fnPayment;
+
+public class fnPayment
+{
+    private readonly ILogger<fnPayment> _logger;
+    private readonly IConfiguration _configuration;
+    private readonly string[] StatusList = { "Aprovado", "Reprovado", "Em análise" };
+    private readonly Random random = new Random();
+
+
+    public fnPayment(ILogger<fnPayment> logger, IConfiguration configuration)
+    {
+        _logger = logger;
+        _configuration = configuration; 
+    }
+
+
+
+    [Function(nameof(fnPayment))]
+    [CosmosDBOutput("%CosmosDB%", "%CosmosContainer%", Connection = "CosmosDBConnection", CreateIfNotExists =true)]
+    public async Task<object?> Run(
+        [ServiceBusTrigger("payment-queue", Connection = "ServiceBusConnection")]
+        ServiceBusReceivedMessage message,
+        ServiceBusMessageActions messageActions)
+    {
+        _logger.LogInformation("Message ID: {id}", message.MessageId);
+        _logger.LogInformation("Message Body: {body}", message.Body);
+        _logger.LogInformation("Message Content-Type: {contentType}", message.ContentType);
+
+        PaymentModel payment = null;
+        try
+        {
+            payment = JsonSerializer.Deserialize<PaymentModel>(message.Body.ToString(), new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+
+            });
+            if (payment == null)
+            {
+                await messageActions.DeadLetterMessageAsync(message, null, "a mensagem nao pode ser deserializada.");
+
+            }
+            int index = random.Next(StatusList.Length);
+            string status = StatusList[index];
+            payment.Status = status;
+
+            if (status == "Aprovado")
+            {
+                payment.DataAprovacao = DateTime.Now;
+                await SentToNotificationQueue(payment);
+
+            }
+            return payment;
+        }
+        catch (Exception ex)
+        {
+            await messageActions.DeadLetterMessageAsync(message, null, $"Erro: {ex.Message}");
+            throw;
+        }
+        finally
+        {
+            await messageActions.CompleteMessageAsync(message);
+
+        }
+
+        // Complete the message
+        await messageActions.CompleteMessageAsync(message);
+    }
+
+    private async Task SentToNotificationQueue(PaymentModel payment)
+    {
+        var connectionString = _configuration.GetSection("ServiceBusConnection").Value.ToString();
+        var queueName = _configuration.GetSection("NotificationQueue").Value.ToString();
+
+        var serviceBusClient = new ServiceBusClient(connectionString);
+        var sender = serviceBusClient.CreateSender(queueName);
+        var message = new ServiceBusMessage(JsonSerializer.Serialize(payment))
+        {
+            ContentType = "application/json",
+            MessageId = payment.IdPayment.ToString()
+        };
+
+        message.ApplicationProperties["IdPayment"] = payment.IdPayment;
+        message.ApplicationProperties["type"] = "notification";
+        message.ApplicationProperties["message"] = "Pagamento Aprovado com sucesso!!";
+
+
+        try
+        {
+            await sender.SendMessageAsync(message);
+            _logger.LogInformation("Mensagem de notificação enviada para a fila: {id}",payment.IdPayment.ToString());
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error ao enviar a mensagem de notificação na fila");
+            throw;
+        }
+        finally
+        {
+            await sender.DisposeAsync();
+            await serviceBusClient.DisposeAsync();  
+        }
+
+    }
+}
---
+++ arquivos/functions/fnPayment/PaymentModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fnPayment.Model
+{
+    internal class PaymentModel
+    {
+        public Guid id { get { return Guid.NewGuid(); } }
+        public Guid IdPayment { get { return Guid.NewGuid(); } }
+        public string nome { get; set; }
+        public string email { get; set; }
+        public string modelo { get; set; }
+        public int ano { get; set; }
+        public string tempoAluguel { get; set; }
+        public DateTime data { get; set; }
+        public string Status {  get; set; }
+        public DateTime? DataAprovacao { get; set; }
+
+    }
+}
